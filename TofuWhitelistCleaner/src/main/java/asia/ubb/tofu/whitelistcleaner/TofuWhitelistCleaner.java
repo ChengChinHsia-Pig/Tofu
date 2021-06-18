09b60dddcbeee0613a5867cf73cd7bb350f70650
Leung Yau Ming <leungyauming2005@gmail.com>
2021-06-18 14:11:11 +0800

    move UBBWhitelistCleaner to TofuWhitelistCleaner module

---
+++ TofuWhitelistCleaner/src/main/java/asia/ubb/tofu/whitelistcleaner/TofuWhitelistCleaner.java
@@ -0,0 +1,47 @@
+package asia.ubb.tofu.whitelistcleaner;
+
+import asia.ubb.tofu.whitelistcleaner.tasks.CleanTask;
+import asia.ubb.tofu.whitelistcleaner.utils.TicksUtils;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+
+public class TofuWhitelistCleaner extends JavaPlugin {
+
+    private final File logFile = new File(getDataFolder(), getConfig().getString("logging.file.filename", "log.txt"));
+
+    private void saveLog() {
+        try {
+            if (!logFile.createNewFile())
+                getLogger().warning("Could not save " + logFile.getName() + " because " + logFile.getName() + " already exists.");
+        } catch (IOException e) {
+            getLogger().log(Level.SEVERE, "Could not save " + logFile.getName() + ".", e);
+        }
+    }
+
+    @Override
+    public void onEnable() {
+        // save default config
+        saveDefaultConfig();
+        // save cleaner log file
+        saveLog();
+
+        // get clean interval from config
+        long cleanInterval = TicksUtils.parseTicks(
+                getConfig().getString("cleaner.interval", "1d"));
+        // get offline allowance from config
+        long offlineAllowance = TicksUtils.parseTicks(
+                getConfig().getString("cleaner.offline-allowance", "3M"));
+        // print clean interval and offline allowance to console
+        getLogger().info("Clean Interval (Seconds):         " + cleanInterval / 20);
+        getLogger().info("Offline Before Clean (Seconds):   " + offlineAllowance / 20);
+
+        // schedule clean whitelist task
+        getServer().getScheduler().scheduleSyncRepeatingTask(
+                this, new CleanTask(this, offlineAllowance, logFile),
+                0L, cleanInterval);
+    }
+
+}
---
+++ TofuWhitelistCleaner/src/main/java/asia/ubb/tofu/whitelistcleaner/tasks/CleanTask.java
@@ -0,0 +1,71 @@
+package asia.ubb.tofu.whitelistcleaner.tasks;
+
+import org.bukkit.OfflinePlayer;
+import org.bukkit.plugin.Plugin;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+
+public class CleanTask implements Runnable {
+
+    private final Plugin plugin;
+    private final long offlineBeforeClean;
+    private final File logFile;
+
+    public CleanTask(Plugin plugin, long offlineBeforeClean, File logFile) {
+        this.plugin = plugin;
+        this.offlineBeforeClean = offlineBeforeClean;
+        this.logFile = logFile;
+    }
+
+    @Override
+    public void run() {
+        // check if whitelist cleaner is enabled
+        if (plugin.getConfig().getBoolean("cleaner.enable", false)) {
+            try {
+                // open log file
+                FileWriter logFileWriter = new FileWriter(logFile, true);
+
+                // get current date and time
+                Date now = new Date();
+                // loop through all whitelisted players
+                for (OfflinePlayer player : plugin.getServer().getWhitelistedPlayers()) {
+                    // get last played time
+                    long lastPlayed = player.getLastPlayed();
+                    // skip the player if the player has never played before
+                    if (plugin.getConfig().getBoolean("cleaner.remove-never-played", false))
+                        if (lastPlayed == 0)
+                            continue;
+                    // calculate player offline time
+                    long offlineTime = (now.getTime() - lastPlayed) / 1000 * 20;
+
+                    // check if player offline time is longer than allowed and if player is not online
+                    if (offlineTime >= offlineBeforeClean && !player.isOnline()) {
+                        // remove player from whitelist
+                        player.setWhitelisted(false);
+                        // reload whitelist
+                        plugin.getServer().reloadWhitelist();
+
+                        // check if file logging is enabled
+                        if (plugin.getConfig().getBoolean("logging.file.enable", false)) {
+                            // convert current time to string
+                            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+                            logFileWriter.write("[" + sdf.format(now) + "] " +
+                                    player.getName() + " (" + player.getUniqueId() + ")" + System.lineSeparator());
+                        }
+                    }
+                }
+
+                // close log file
+                logFileWriter.close();
+            } catch (IOException e) {
+                plugin.getLogger().log(Level.SEVERE, "Could not open or write to " + logFile.getName() + ".", e);
+            }
+        }
+    }
+
+}
