7181ba44f7c84c01ccabcbcc8dd280e57d7fe190
Leung Yau Ming <leungyauming2005@gmail.com>
2021-06-18 21:17:52 +0800

    TofuFramework: create LogService

---
+++ TofuFramework/src/main/java/asia/ubb/tofu/framework/LogService.java
@@ -0,0 +1,32 @@
+package asia.ubb.tofu.framework;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class LogService {
+
+    private final Logger logger;
+
+    public LogService(JavaPlugin plugin) {
+        logger = plugin.getLogger();
+    }
+
+    public void setLevel(Level level) {
+        logger.setLevel(level);
+    }
+
+    public void info(String msg, Object... args) {
+        logger.info(String.format(msg, args));
+    }
+
+    public void warning(String msg, Object... args) {
+        logger.warning(String.format(msg, args));
+    }
+
+    public void error(String msg, Object... args) {
+        logger.severe(String.format(msg, args));
+    }
+
+}
