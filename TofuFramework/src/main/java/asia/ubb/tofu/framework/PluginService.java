ca9b92613d7edee902da1b4be9eb9018299b85bb
Leung Yau Ming <leungyauming2005@gmail.com>
2021-06-19 11:27:13 +0800

    TofuFramework: create PluginService

---
+++ TofuFramework/src/main/java/asia/ubb/tofu/framework/PluginService.java
@@ -0,0 +1,20 @@
+package asia.ubb.tofu.framework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PluginService {
+
+    private List<Runnable> reloadCallbacks = new ArrayList<>();
+
+    public void registerReloadCallback(Runnable callback) {
+        reloadCallbacks.add(callback);
+    }
+
+    public void reload() {
+        for (Runnable callback : reloadCallbacks) {
+            callback.run();
+        }
+    }
+
+}
