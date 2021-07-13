e22b5b5668bb79d68f26371615a05a52dba2bdbd
Leung Yau Ming <leungyauming2005@gmail.com>
2021-07-13 16:06:06 +0800

    fix(TofuJoinCooldown): fix PlayerLoginEventListener not found

--- TofuJoinCooldown/src/main/java/asia/ubb/tofu/joincooldown/listeners/PlayerLoginEventListener.java
+++ TofuJoinCooldown/src/main/java/asia/ubb/tofu/joincooldown/listeners/PlayerLoginEventListener.java
@@ -4,28 +4,28 @@ import asia.ubb.tofu.joincooldown.services.CooldownService;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
-public class PlayerJoinEventListener implements Listener {
+public class PlayerLoginEventListener implements Listener {
 
     private final long cooldownSecond;
 
-    public PlayerJoinEventListener(long cooldownSecond) {
+    public PlayerLoginEventListener(long cooldownSecond) {
         this.cooldownSecond = cooldownSecond;
     }
 
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
+    public void onPlayerLogin(PlayerLoginEvent event) {
         Player player = event.getPlayer();
         LocalDateTime playerOfflineTime = CooldownService.getPlayerOfflineTime(player);
         if (playerOfflineTime != null) {
             if (playerOfflineTime.until(LocalDateTime.now(), ChronoUnit.SECONDS) >= cooldownSecond) {
                 CooldownService.removePlayerOfflineTime(event.getPlayer());
             } else {
-                player.kickPlayer(String.format("Please wait %d seconds before joining the server again!", cooldownSecond));
+                event.disallow(PlayerLoginEvent.Result.KICK_OTHER, String.format("Please wait %d seconds before joining the server again!", cooldownSecond));
             }
         }
     }
